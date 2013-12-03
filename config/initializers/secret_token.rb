1a04eafd4dfb804d6522e0f99ee27166fb7a6fe7
Craig Morrison <craig@craigmorrison.ca>
2013-12-03 16:48:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pixilsnap::Application.config.secret_key_base = '7877ba35a13b086a2d33ce4a675c10f22bc1735e8340ba6b4936aa733484284c7ab9e07e459026e23ee3a7281af6ef75783898fbfe4246d616f46a61ffc838d5'
