dc7a17dde66f172dcc307529e78931688360fdbe
Craig Morrison <craig@craigmorrison.ca>
2013-12-04 17:19:37 -0500

    attempting to set default roots for authenticated users, not completed yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Pixilsnap::Application.routes.draw do
   devise_for :users
   root "pages#home"
   get "userPage" => "pages#userPage"
+
+
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
